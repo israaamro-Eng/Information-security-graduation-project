490f1097d9ee5ec24d77293c8c37d8bfb8ece20f
Israa Amro <israaamro0568@gmail.com>
2023-12-18 14:44:46 +0200

    Edit

--- assets/java/script1.js
+++ assets/java/script1.js
@@ -1,3 +1,48 @@
+
+//............start aside .........
+
+
+let menuItem = document.querySelectorAll('.menu-item');
+
+//active class remove...
+const removeActive = ()=>{
+    menuItem.forEach(item=>{
+        item.classList.remove('active')
+    })
+}
+
+menuItem.forEach(item =>{
+    item.addEventListener('click',()=>{
+        removeActive();
+        item.classList.add('active');
+        document.querySelector('.notification-box').style.display='none'
+    })
+})
+
+document.querySelector('#Notify-box').addEventListener('click',()=>{
+    document.querySelector('.notification-box').style.display='block'
+    document.querySelector('#ntcounter1').style.display='none'
+})
+
+document.querySelector('#askMenu').addEventListener('click',()=>{
+    document.querySelector('#notfycounter2').style.display='none'
+})
+
+document.querySelector('#messageMenu').addEventListener('click',()=>{
+    document.querySelector('#notfycounter3').style.display='none'
+})
+
+document.querySelector('#drMessageMenu').addEventListener('click',()=>{
+    document.querySelector('#notfycounter4').style.display='none'
+})
+
+
+
+
+
+
+
+
 //............start profile popup.........
 document.querySelectorAll('#my-profile-picture').forEach(AllProfile => {
     AllProfile.addEventListener('click',()=>{
@@ -9,6 +54,8 @@ document.querySelectorAll('#my-profile-picture').forEach(AllProfile => {
 document.querySelectorAll('.close').forEach(AllClose => {
     AllClose.addEventListener('click',()=>{
         document.querySelector('.profile-popup').style.display='none'
+        document.querySelector('.add-post-popup').style.display='none'
+        document.querySelector('.theme-customize').style.display='none'
 
     })
 });
@@ -22,4 +69,24 @@ document.querySelector('#profile-upload').addEventListener('change',()=>{
 //............window scroll...........
 window.addEventListener('scroll',() =>{
     document.querySelector('.profile-popup').style.display="none"
+    document.querySelector('.add-post-popup').style.display='none'
+    document.querySelector('.theme-customize').style.display='none'
+    document.querySelector('.notification-box').style.display='none'
+})
+
+//............start add post popup.........
+document.querySelector('#create-lg').addEventListener('click',()=> {
+        document.querySelector('.add-post-popup').style.display='flex'
+});
+
+
+document.querySelector('#feed-pic-upload').addEventListener('change',()=>{
+    document.querySelector('#postIMG').src = URL.createObjectURL(document.querySelector('#feed-pic-upload').files[0]);
+})
+
+//...........liked button..............
+document.querySelectorAll('.action-button span:first-child i').forEach(liked=>{
+    liked.addEventListener('click',()=>{
+        liked.classList.toggle('liked');
+    })
 })
